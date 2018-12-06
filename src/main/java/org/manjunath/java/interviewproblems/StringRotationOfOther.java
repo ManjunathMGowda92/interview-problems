54b53784a4ee28258f3353cb7ed9d9a718a58deb
Manjunath HM <mahm0001@synchronoss.com>
2018-12-06 12:38:21 +0530

    Added new java program for rotation of strings and also test case for same

---
+++ src/main/java/org/manjunath/java/interviewproblems/StringRotationOfOther.java
@@ -0,0 +1,30 @@
+package org.manjunath.java.interviewproblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringRotationOfOther {
+	
+	private List<String> getRotationList(String str) {
+		List<String> rotationList = new ArrayList<>();
+		String temp = str;
+		
+		if (str != null) {
+			rotationList.add(str);
+			for (int i = 0; i < str.length()-1; i++) {
+				temp = str.substring(i+1) + str.substring(0, i+1);
+				rotationList.add(temp);
+			}
+		}
+		return rotationList;
+	}
+	
+	public boolean isStringRotationOfOther(String str1, String str2) {	
+		List<String> rotationList = getRotationList(str1);
+		for(String str : rotationList) {
+			if (str.equals(str2))
+				return true;
+		}				
+		return false;
+	}
+}
