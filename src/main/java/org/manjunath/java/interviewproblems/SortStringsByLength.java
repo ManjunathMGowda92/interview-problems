7ebabf54723b6dd40cd289d8052521a9da29dc2b
Manjunath HM <mahm0001@synchronoss.com>
2018-12-14 11:50:15 +0530

    Code for Sorting of Strings using length and respective test cases

---
+++ src/main/java/org/manjunath/java/interviewproblems/SortStringsByLength.java
@@ -0,0 +1,40 @@
+package org.manjunath.java.interviewproblems;
+
+public class SortStringsByLength {
+	
+	private void quickSort(String[] arr, int start, int end) {
+		if (start > end)
+			return;
+		if (start <= end) {
+			int partitionIndex = getPartitionIndex(arr, start, end);
+			quickSort(arr, start, partitionIndex - 1);
+			quickSort(arr, partitionIndex + 1, end);
+		}
+	}
+	
+	private int getPartitionIndex(String[] arr, int start, int end) {
+		int partitionIndex = start;
+		String pivot = arr[end];
+		
+		for (int i = start; i < end; i++) {
+			if (arr[i].length() <= pivot.length()) {
+				String temp = arr[i];
+				arr[i] = arr[partitionIndex];
+				arr[partitionIndex] = temp;
+				
+				partitionIndex++;				
+			}
+		}
+		
+		String temp = arr[partitionIndex];
+		arr[partitionIndex] = arr[end];
+		arr[end] = temp;
+		return partitionIndex;
+	}
+	
+	public String[] getSortedStringArrayByLength(String[] strArr) {
+		quickSort(strArr, 0, strArr.length-1);
+		
+		return strArr;
+	}
+}
---
+++ src/test/java/org/manjunath/test/interviewproblems/TestSortStringsByLength.java
@@ -0,0 +1,61 @@
+package org.manjunath.test.interviewproblems;
+
+import org.manjunath.java.documentation.TestCase;
+import org.manjunath.java.interviewproblems.SortStringsByLength;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestSortStringsByLength {
+	private SortStringsByLength sortArr;
+	
+	@BeforeClass
+	public void doInitialization() {
+		sortArr = new SortStringsByLength();
+	}
+	
+	@Test(testName = "testSortStringArray1")
+	@TestCase(author = "Manjunath HM",
+	testCaseDescription = "to test getSortedStringArrayByLength() method and to get resulted array in sorted order of String length ",
+	expectedResult = "getSortedStringArrayByLength() method should return sorted String array")
+	public void testSortStringArray1() {
+		Reporter.log("TestSortStringsByLength: Start of testSortStringArray1() method", true);
+		try {
+			String[] inputArr = {"Manjunath", "Manoj", "Deepa", "Suprabha", "RangaSwamy", "Sunil", "Vinay", "Santhosh", "Raju", "Suhas", "Akshitha"};
+			String[] sortedArr = sortArr.getSortedStringArrayByLength(inputArr);
+			String[] expectedArr = {"Raju", "Deepa", "Sunil", "Vinay", "Manoj", "Suhas", "Suprabha", "Santhosh", "Akshitha", "Manjunath", "RangaSwamy"};
+			
+			Assert.assertEquals(sortedArr, expectedArr);
+		} catch (AssertionError e) {
+			Reporter.log("AssertionError occurred:"+e, true);
+			Assert.assertTrue(false);
+		} catch (Exception e) {
+			Reporter.log("AssertionError occurred:"+e, true);
+			Assert.assertTrue(false);
+		}
+		Reporter.log("TestSortStringsByLength: End of testSortStringArray1() method", true);
+	}
+	
+	@Test(testName = "testSortStringArray2")
+	@TestCase(author = "Manjunath HM",
+	testCaseDescription = "to test getSortedStringArrayByLength() method and to get resulted array in sorted order of String length ",
+	expectedResult = "getSortedStringArrayByLength() method should return sorted String array")
+	public void testSortStringArray2() {
+		Reporter.log("TestSortStringsByLength: Start of testSortStringArray2() method", true);
+		try {
+			String[] inputArr = {"123", "12345", "1", "12345678", "1234", "12", "1234567", "123456"};
+			String[] sortedArr = sortArr.getSortedStringArrayByLength(inputArr);
+			String[] expectedArr = {"1", "12", "123", "1234", "12345", "123456", "1234567", "12345678"};
+			
+			Assert.assertEquals(sortedArr, expectedArr);
+		} catch (AssertionError e) {
+			Reporter.log("AssertionError occurred:"+e, true);
+			Assert.assertTrue(false);
+		} catch (Exception e) {
+			Reporter.log("AssertionError occurred:"+e, true);
+			Assert.assertTrue(false);
+		}
+		Reporter.log("TestSortStringsByLength: End of testSortStringArray2() method", true);
+	}
+}
